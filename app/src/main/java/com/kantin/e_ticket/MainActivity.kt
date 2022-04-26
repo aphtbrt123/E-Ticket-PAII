817ad967650343b7616643590916212bd1a5ca6b
aphtbrt123 <andrenando2002@gmail.com>
2022-04-26 17:41:15 +0700

    Button Nav Bar 1

--- app/src/main/java/com/kantin/e_ticket/MainActivity.kt
+++ app/src/main/java/com/kantin/e_ticket/MainActivity.kt
@@ -2,10 +2,65 @@ package com.kantin.e_ticket
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import androidx.core.view.get
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.kantin.e_ticket.fragment.AkunFragment
+import com.kantin.e_ticket.fragment.HomeFragment
+import com.kantin.e_ticket.fragment.TiketFragment
 
 class MainActivity : AppCompatActivity() {
+
+    private val fragmentHome: Fragment = HomeFragment()
+    private val fragmentTiket: Fragment = TiketFragment()
+    private val fragmentAkun: Fragment = AkunFragment()
+    private val fm: FragmentManager = supportFragmentManager
+    private var active: Fragment = fragmentHome
+
+    private lateinit var menu: Menu
+    private lateinit var menuItem: MenuItem
+    private lateinit var bottomNavigationView: BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        fm.beginTransaction().add(R.id.container, fragmentHome).show(fragmentHome).commit()
+        fm.beginTransaction().add(R.id.container, fragmentTiket).hide(fragmentTiket).commit()
+        fm.beginTransaction().add(R.id.container, fragmentAkun).hide(fragmentAkun).commit()
+
+        bottomNavigationView = findViewById(R.id.nav_view)
+        menu = bottomNavigationView.menu
+        menuItem = menu.getItem(0)
+        menuItem.isChecked = true
+
+        bottomNavigationView.setOnNavigationItemSelectedListener { item ->
+            when(item.itemId){
+                R.id.navigation_home ->{
+                    callFragment(0,fragmentHome)
+                }
+
+                R.id.navigation_tiket ->{
+                    callFragment(1,fragmentTiket)
+                }
+
+                R.id.navigation_akun ->{
+                    callFragment(2, fragmentAkun)
+                }
+            }
+
+            false
+        }
+    }
+
+    fun callFragment(int: Int, fragment: Fragment){
+        menuItem = menu.getItem(int)
+        menuItem.isChecked = true
+        fm.beginTransaction().hide(active).show(fragment).commit()
+        active = fragmentAkun
     }
 }
