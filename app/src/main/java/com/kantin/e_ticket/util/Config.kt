6ade8dc1938ae9d1019dfec61485405a32083091
matthewalfredoo <matthewwalfredoo@gmail.com>
2022-06-27 03:12:25 +0700

    Update Config.kt

--- app/src/main/java/com/kantin/e_ticket/util/Config.kt
+++ app/src/main/java/com/kantin/e_ticket/util/Config.kt
@@ -1,7 +1,7 @@
 package com.kantin.e_ticket.util
 
 object Config {
-    /*const val baseUrl = "http://192.168.43.104/"*/
-    const val baseUrl = "http://192.168.100.10:8000/"
+    const val baseUrl = "http://192.168.43.104/"
+    /*const val baseUrl = "http://192.168.100.10:8000/"*/
     const val ticketUrl = baseUrl
 }
